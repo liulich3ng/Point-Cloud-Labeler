68d35d7aa3c90b7ed18707b707046a354e41814d
allele <liulich3ng@outlook.com>
2023-03-29 21:20:06 +0800

    feat: select label

---
+++ src/components/LabelSelect.vue
@@ -0,0 +1,21 @@
+<template>
+  <n-select v-model:value="currentLabelIndex"
+            :options="options"></n-select>
+</template>
+
+<script setup lang="ts">
+import {currentLabelIndex} from '@/store/annotations.js';
+import {labels} from "@/config/labels";
+import {NSelect} from 'naive-ui'
+
+const options = labels.map((label, index) => {
+  return {
+    label: label.name,
+    value: index,
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
