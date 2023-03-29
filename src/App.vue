80e234e3b6f87c3025ff87bfea8c835b720f3ad0
allele <liulich3ng@outlook.com>
2023-03-30 01:54:39 +0800

    feat: UI

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,36 @@
 <template>
-  <div class="container">
-    <app-header class="header"></app-header>
-    <div class="main">
-      <div class="sidebar"></div>
-      <div class="editor"></div>
+  <n-config-provider :theme="darkTheme" :locale="zhCN" :date-locale="dateZhCN">
+    <div class="container">
+      <app-header/>
+      <div class="main">
+        <app-side-bar/>
+        <app-editor/>
+      </div>
+      <app-status-bar/>
     </div>
-    <div class="statusbar">
-      <p>Line 1, Column 1</p>
-      <p>UTF-8</p>
-    </div>
-  </div>
+  </n-config-provider>
 </template>
 
 <script setup lang="ts">
+import {darkTheme, NConfigProvider, zhCN, dateZhCN} from 'naive-ui'
 import AppHeader from "@/components/AppHeader.vue"
+import AppSideBar from "@/components/AppSideBar.vue"
+import AppEditor from "@/components/AppEditor.vue"
+import AppStatusBar from "@/components/AppFooter.vue"
+import {loadPCD} from "@/functions/usePCD";
+
+loadPCD()
 </script>
 
-<style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
+<style scoped>
 .container {
   display: flex;
   flex-direction: column;
   height: 100vh;
 }
 
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: #1e1e1e;
-  color: #fff;
-  padding: 0 20px;
-  height: 50px;
-}
-
 .main {
-  flex: 1;
+  flex: auto;
   display: flex;
-}
-
-.sidebar {
-  display: flex;
-  flex-direction: column;
-  background-color: #252526;
-  color: #fff;
-  padding: 20px;
-  width: 200px;
-}
-
-.editor {
-  flex: 1;
-  background-color: #1e1e1e;
-  color: #fff;
-  padding: 20px;
-}
-
-.statusbar {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: #007acc;
-  color: #fff;
-  padding: 0 20px;
-  height: 20px;
 }
 </style>
