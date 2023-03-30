2a76bdfe0934d2447cd9ace08aafaf87f7f1bdb7
allele <liulich3ng@outlook.com>
2023-03-30 20:34:32 +0800

    style:

--- src/components/AppEditor.vue
+++ src/components/AppEditor.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="editor">
-    <div id="perspective"></div>
-    <div class="three">
-      <div id="top">top</div>
-      <div id="main">main</div>
-      <div id="left">left</div>
+    <div id="editor">
+        <div id="perspective"></div>
+        <div class="three">
+            <div id="top">top</div>
+            <div id="main">main</div>
+            <div id="left">left</div>
+        </div>
+        <canvas id="canvas"></canvas>
     </div>
-    <canvas id="canvas"></canvas>
-  </div>
 </template>
 
 <script setup lang="ts">
@@ -18,33 +18,33 @@ onMounted(initCanvas)
 </script>
 
 <style scoped>
-.editor {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  display: flex;
+#editor {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    display: flex;
 }
 
 #perspective {
-  flex: 1 1 auto;
-  border-right: 1px solid var(--border-color);
+    flex: 1 1 auto;
+    border-right: 1px solid var(--border-color);
 }
 
 .three {
-  flex: 0 0 240px;
-  display: flex;
-  flex-direction: column;
+    flex: 0 0 240px;
+    display: flex;
+    flex-direction: column;
 }
 
 #top, #main, #left {
-  flex: 1;
+    flex: 1;
 }
 
 #canvas {
-  z-index: -1;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  display: block;
+    z-index: -1;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    display: block;
 }
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,25 +1,25 @@
 <template>
-  <div class="header">
-    <n-space>
-      <ghost-icon tooltip="menu">
-        <reorder-four/>
-      </ghost-icon>
-      <ghost-icon tooltip="default" :pressed="mode===MODE.default"
-                  @click="mode=MODE.default">
-        <navigate-outline/>
-      </ghost-icon>
-      <ghost-icon tooltip="add" :pressed="mode===MODE.put"
-                  @click="mode=MODE.put"
-      >
-        <add/>
-      </ghost-icon>
-      <label-select style="width: 100px"></label-select>
-      <n-divider vertical></n-divider>
-      <ghost-icon tooltip="reset camera">
-        <camera-reverse-outline/>
-      </ghost-icon>
-    </n-space>
-  </div>
+    <div class="header">
+        <n-space>
+            <ghost-icon tooltip="menu">
+                <reorder-four/>
+            </ghost-icon>
+            <ghost-icon tooltip="default" :pressed="mode===MODE.default"
+                        @click="mode=MODE.default">
+                <navigate-outline/>
+            </ghost-icon>
+            <ghost-icon tooltip="add" :pressed="mode===MODE.put"
+                        @click="mode=MODE.put"
+            >
+                <add/>
+            </ghost-icon>
+            <label-select style="width: 100px"></label-select>
+            <n-divider vertical></n-divider>
+            <ghost-icon tooltip="reset camera" @click="resetCamera">
+                <camera-reverse-outline/>
+            </ghost-icon>
+        </n-space>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -31,17 +31,18 @@ import {
 } from '@vicons/ionicons5'
 import {NSpace, NDivider} from 'naive-ui'
 import GhostIcon from "@/components/GhostIcon.vue";
-import {mode} from "@/store/item";
-import {MODE} from "@/types/draw";
+import {mode} from "@/store/global";
+import {MODE} from "@/types/global";
 import LabelSelect from "@/components/LabelSelect.vue";
+import {resetCamera} from "@/functions/useCanvas";
 </script>
 
 <style scoped>
 .header {
-  flex: 0 0 var(--header-height);
-  border-bottom: 1px solid var(--border-color);
-  padding-left: 40px;
-  display: flex;
-  align-items: center;
+    flex: 0 0 var(--header-height);
+    border-bottom: 1px solid var(--border-color);
+    padding-left: 40px;
+    display: flex;
+    align-items: center;
 }
 </style>
