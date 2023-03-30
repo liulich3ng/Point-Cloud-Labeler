10ad584d046f32e387132e9400824c8d6e8e36f2
allele <liulich3ng@outlook.com>
2023-03-30 23:27:24 +0800

    feat: simple Object List

---
+++ src/components/ObjectList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div v-for="obj in annotationObjectsAtCurrentFrame">
+            {{ obj.id }}
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import {
+  annotationObjectsAtCurrentFrame
+} from "@/store/annotations";
+
+</script>
+
+<style scoped>
+
+</style>
