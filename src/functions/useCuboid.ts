fbb59f4da19c56880097ceb9db13bc9219e2d25e
allele <liulich3ng@outlook.com>
2023-04-09 18:50:37 +0800

    refactor:

--- src/functions/useCuboid.ts
+++ src/functions/useCuboid.ts
@@ -12,17 +12,18 @@ import {
  * @param color
  */
 export function makeCuboid(points: number[] = [1, 1, 1, 0, 0, 0, 0, 0, 0], color = '#0000FF') {
-  const geometry = new BoxGeometry(points[0], points[1], points[2]);
+  const geometry = new BoxGeometry();
   const material = new MeshBasicMaterial({
     color: color,
     transparent: true,
     opacity: 0.4
-  })
+  });
   const cube = new Mesh(geometry, material);
   const edgesGeometry = new EdgesGeometry(geometry);
   const edgesMaterial = new LineBasicMaterial({color: color});
   const edges = new LineSegments(edgesGeometry, edgesMaterial);
   cube.add(edges);
+  cube.scale.set(points[0], points[1], points[2]);
   cube.position.set(points[3], points[4], points[5]);
   cube.rotation.set(points[6], points[7], points[8]);
   return cube;
