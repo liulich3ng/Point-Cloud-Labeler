bb4d3306c7243bcdf5cbb23290068eee8454ef78
allele <liulich3ng@outlook.com>
2023-04-09 18:51:12 +0800

    feat: drag box

--- src/functions/useMouse.ts
+++ src/functions/useMouse.ts
@@ -1,69 +1,70 @@
-import {currentFrame, mode} from "@/store/global";
+import {currentFrame, groundHeight, mode} from "@/store/global";
 import {MODE} from "@/types/global";
 import {render} from "@/functions/useRender";
 import {annotationObjects, currentLabel} from "@/store/annotations";
 import {Shape} from "@/cores/annotations";
 import {Raycaster, Vector2, Vector3} from "three";
 import {drawHelper} from "@/functions/useScene";
-import {perspectiveCamera} from "@/functions/useCamera";
+import {perspectiveCamera, perspectiveControl} from "@/functions/useCamera";
 
 const raycaster = new Raycaster();
 const mouseCurrent = new Vector2();
 const mouseBegin = new Vector2();
+const mouseBeginToGround = new Vector3();
 const mouseEnd = new Vector2();
 
 
 export function onPerspectiveViewMousemove(e: MouseEvent) {
-  if (mode.value === MODE.default) {
-    computeCurrentMouse(e);
-    // raycaster.setFromCamera(mouseCurrent, perspectiveCamera);
-    // const intersectObjects = raycaster.intersectObject(POINTS.value, false);
-    // if (intersectObjects.length > 0) {
-    //   render();
-    // } else {
-    //
-    // }
-  } else if (mode.value === MODE.put) {
-    computeCurrentMouse(e);
-    moveToMousePosition();
-    render();
-  } else if (mode.value === MODE.drag) {
-
+  computeCurrentMouse(e);
+  switch (mode.value) {
+    case MODE.default:
+      break;
+    case MODE.put:
+      drawHelper.position.copy(mouseToGround());
+      break;
+    case MODE.drag:
+      if (e.buttons !== 1) return;
+      dragBox();
+      break;
   }
+  render();
 }
 
 export function onPerspectiveViewMousedown(e: MouseEvent) {
-  if (mode.value === MODE.put) {
-    const points: number[] = [
-      drawHelper.geometry.parameters.width,
-      drawHelper.geometry.parameters.height,
-      drawHelper.geometry.parameters.depth,
-      drawHelper.position.x,
-      drawHelper.position.y,
-      drawHelper.position.z,
-      drawHelper.rotation.x,
-      drawHelper.rotation.x,
-      drawHelper.rotation.x,
-    ];
-    const shape = new Shape(currentLabel.value, currentFrame.value, points);
-    annotationObjects.push(shape);
-    mode.value = MODE.default;
-    render();
-  } else if (mode.value === MODE.drag) {
-
+  switch (mode.value) {
+    case MODE.default:
+      break;
+    case MODE.put:
+      break;
+    case MODE.drag:
+      mouseBeginToGround.copy(mouseToGround());
+      break;
   }
 }
 
 export function onPerspectiveViewMouseup(e: MouseEvent) {
-  if (mode.value === MODE.drag) {
-
+  switch (mode.value) {
+    case MODE.default:
+      break;
+    case MODE.put:
+      if (e.button !== 0) return;
+      finishDraw();
+      mode.value = MODE.default;
+      render();
+      break;
+    case MODE.drag:
+      if (e.button !== 0) return;
+      finishDraw();
+      mode.value = MODE.default;
+      render();
+      break;
   }
 }
 
 export function onPerspectiveViewWheel(e: WheelEvent) {
   if (mode.value === MODE.put) {
     computeCurrentMouse(e);
-    moveToMousePosition();
+    drawHelper.position.copy(mouseToGround());
     render();
   }
 }
@@ -71,22 +72,57 @@ export function onPerspectiveViewWheel(e: WheelEvent) {
 /**
  * let drawHelper move with mouse position
  */
-function moveToMousePosition() {
+function mouseToGround() {
   const vec = new Vector3(mouseCurrent.x, mouseCurrent.y, 0)
     .unproject(perspectiveCamera)
     .sub(perspectiveCamera.position)
     .normalize();
 
-  const factor = -perspectiveCamera.position.z / vec.z;
+  const factor = (groundHeight.value - perspectiveCamera.position.z) / vec.z;
 
   const pos = new Vector3()
     .copy(perspectiveCamera.position)
     .add(vec.multiplyScalar(factor));
-  drawHelper.position.set(pos.x, pos.y, 0);
+  return pos.setZ(groundHeight.value);
 }
 
+/**
+ * mouse position to camera's normalized device coordinate (NDC)
+ * @param e
+ */
 function computeCurrentMouse(e: MouseEvent) {
   const freeView = document.getElementById('perspective') as HTMLElement;
   mouseCurrent.x = (e.offsetX / freeView.clientWidth) * 2 - 1;
   mouseCurrent.y = -(e.offsetY / freeView.clientHeight) * 2 + 1;
+}
+
+function finishDraw() {
+  const points: number[] = [
+    drawHelper.scale.x,
+    drawHelper.scale.y,
+    drawHelper.scale.z,
+    drawHelper.position.x,
+    drawHelper.position.y,
+    drawHelper.position.z,
+    drawHelper.rotation.x,
+    drawHelper.rotation.y,
+    drawHelper.rotation.z,
+  ];
+  const shape = new Shape(currentLabel.value, currentFrame.value, points);
+  annotationObjects.push(shape);
+}
+
+function dragBox() {
+  const mouseCurrentToGround = mouseToGround();
+  const angle1 = perspectiveControl.getAzimuthalAngle();
+  const angle2 = Math.atan((mouseCurrentToGround.y - mouseBeginToGround.y) / (mouseCurrentToGround.x - mouseBeginToGround.x));
+  const angle3 = angle2 - angle1;
+  const diagonal = mouseCurrentToGround.distanceTo(mouseBeginToGround);
+  const w = diagonal * Math.cos(angle3);
+  const h = diagonal * Math.sin(angle3);
+  drawHelper.scale.set(w, h, currentLabel.value.defaultSize.z);
+  drawHelper.position.copy(mouseCurrentToGround.add(mouseBeginToGround).divideScalar(2));
+  drawHelper.position.add(new Vector3(0, 0, currentLabel.value.defaultSize.z / 2));
+  // rotate with Z axis
+  drawHelper.setRotationFromAxisAngle(new Vector3(0, 0, 1), angle1);
 }
