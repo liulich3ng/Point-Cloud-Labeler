c4b1496b56d3b1886b09854a63724fb41766a3e8
allele <liulich3ng@outlook.com>
2023-04-14 00:50:45 +0800

    refactor:

--- src/functions/useListener.ts
+++ src/functions/useListener.ts
@@ -1,20 +1,26 @@
-import {
-  onPerspectiveViewMousedown,
-  onPerspectiveViewMousemove,
-  onPerspectiveViewMouseup,
-  onPerspectiveViewWheel
-} from "@/functions/useMouse";
 import {onKeyDown} from "@/functions/useKeyboard";
 import {render} from "@/functions/useRender";
+import {computeCurrentMouse} from "@/functions/useMouse";
 
 export function initEventListeners() {
   window.addEventListener('resize', render, false);
 
   const perspective = document.getElementById('perspective') as HTMLElement;
-  perspective.addEventListener('mousemove', onPerspectiveViewMousemove);
-  perspective.addEventListener('mousedown', onPerspectiveViewMousedown);
-  perspective.addEventListener('mouseup', onPerspectiveViewMouseup);
-  perspective.addEventListener('wheel', onPerspectiveViewWheel);
+  perspective.addEventListener('mousemove', onMousemove);
+  perspective.addEventListener('mousedown', onMousedown);
+  perspective.addEventListener('mouseup', onMouseup);
 
   document.addEventListener('keydown', onKeyDown);
+
+  function onMousemove(e: MouseEvent) {
+    computeCurrentMouse(e);
+  }
+
+  function onMousedown(e: MouseEvent){
+
+  }
+
+  function onMouseup(e:MouseEvent){
+
+  }
 }
